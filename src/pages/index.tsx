60beab4e9880cc3ebb9c273299fb31b395f83543
Meaookung144 <kittinanatthasiri@gmail.com>
2024-11-22 00:26:59 +0700

    assdasd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,24 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 export default function Home() {
   const router = useRouter()
 
   useEffect(() => {
-    router.push('https://mwit.ac.th/')
-  }, [])
+    if (window.location.href !== 'https://mwit.ac.th/') {
+      router.push('https://mwit.ac.th/')
+    }
+  }, [router])
+
   return (
     <main className="">
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Image src="/logo.png" alt="logo" width={100} height={100} />
         <h1 className="text-6xl font-bold text-center">
-          Redirecting...
+          Welcome to MWIT
         </h1>
       </div>
     </main>
-  );
+  )
 }
