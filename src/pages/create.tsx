a3b1c7672f248e68af0da80facceac38011a600a
modanub <sirawich.dam_g34@mwit.ac.th>
2024-12-02 00:26:13 +0700

    major

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,5 +1,5 @@
 // components/ShortLinkForm.tsx
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import useSWR from 'swr';
 import { PrismaClient } from '@prisma/client';
 
@@ -34,12 +34,15 @@ const ShortLinkForm = () => {
   const [redirectUrl, setRedirectUrl] = useState('');
   const { data: shortlinks, mutate } = useSWR<Data[]>('/api/getshortlink', fetcher);
 
-  if (!shortlinks) {
-    return <p>Loading...</p>;
-  }
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!shortlinks) return;
+
+    if (!password || !shortlink || !redirectUrl) {
+      alert('Please fill in all fields');
+      return;
+    }
 
     const newLink = {
       shortlink,
@@ -66,71 +69,108 @@ const ShortLinkForm = () => {
 
   return (
     <div className="card px-5 lg:px-20 pt-5 lg:pt-20">
-      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">PWD</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="··········" required
-          />
-        </div>
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Short Link</label>
-          <input
-            type="text"
-            value={shortlink}
-            onChange={(e) => setShortlink(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="register"
-          />
-        </div>
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Redirect Link</label>
-          <input
-            type="text"
-            value={redirectUrl}
-            onChange={(e) => setRedirectUrl(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://openhouse.mwit.ac.th/register" required
-          />
-        </div>
-        
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Short Link</button>
+      <form className="max-w-sm mx-auto bg-gray-800 p-6 rounded-lg shadow-md" onSubmit={handleSubmit}>
+      <h2 className="text-lg font-bold text-gray-300 mb-5">
+        Create Short Link
+      </h2>
+
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-bold text-gray-300">Password</label>
+        <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="bg-gray-700 border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="··········" required
+        />
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-bold text-gray-300">Short Link</label>
+        <input
+        type="text"
+        value={shortlink}
+        onChange={(e) => setShortlink(e.target.value)}
+        className="bg-gray-700 border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="register"
+        />
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-bold text-gray-300">Redirect Link</label>
+        <input
+        type="text"
+        value={redirectUrl}
+        onChange={(e) => setRedirectUrl(e.target.value)}
+        className="bg-gray-700 border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="https://openhouse.mwit.ac.th/register" required
+        />
+      </div>
+      
+      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Create Short Link</button>
       </form>
 
-      <div className="mt-10">
-        <h2 className="text-lg font-bold">Existing Short Links</h2>
-        {
-          shortlinks.map((link) => (
-            <div key={link.shortlink} className="flex items-center justify-between border-b border-gray-200 dark:border-gray-600 py-2.5">
-              <div>
-                <p className="text-blue-700 dark:text-blue-500 font-bold">
-                  Link: https://tiny.mwit.link/{link.shortlink} 
-                  <button
-                    onClick={(e) => {
-                      navigator.clipboard.writeText(`https://tiny.mwit.link/${link.shortlink}`);
-                      if (e.currentTarget) {
-                        e.currentTarget.textContent = 'Copied';
-                        setTimeout(() => {
-                          if (e.currentTarget) {
-                            e.currentTarget.textContent = 'Copy';
-                          }
-                        }, 2000);
-                      }
-                    }}
-                    className="text-xs ml-2 text-blue-700 dark:text-blue-500 hover:underline"
-                  >
-                    {/* <small>Copy</small> */}
-                    Copy
-                  </button>
-                </p>
-                
-                <p className="text-gray-500 dark:text-gray-400">Redirect to: {link.redirectUrl}</p>
-              </div>
-              <p className="text-gray-500 dark:text-gray-400">Usage: {link.count} clicks</p>
+      <div className="mt-10 max-w-sm mx-auto bg-gray-800 p-6 rounded-lg shadow-md">
+      <h2 className="text-lg font-bold text-gray-300 mb-5">Existing Short Links</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className="list-disc pl-5 max-w-md">
+        {shortlinks?.map((link) => (
+          <li key={link.shortlink} className="mb-2">
+          <div className="flex items-center justify-between">
+            <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              navigator.clipboard.writeText(`https://tiny.mwit.link/${link.shortlink}`);
+              const target = e.currentTarget;
+              target.textContent = 'Shortlink copied';
+              setTimeout(() => {
+              target.textContent = 'https://tiny.mwit.link/' + link.shortlink;
+              }, 2000);
+            }}
+            className="text-blue-400 hover:underline"
+            >
+            {'https://tiny.mwit.link/' + link.shortlink}
+            </a>
+            <div>
+            <span className="text-gray-400">({link.count} clicks)</span>
+            {/* <button
+              onClick={async () => {
+              const response = await fetch(`/api/shortlinks/${link.shortlink}`, {
+                method: 'DELETE',
+                headers: {
+                'Authorization': await sha256(password)
+                }
+              });
+              if (response.ok) {
+                alert('Short link deleted successfully!');
+                mutate(shortlinks.filter((l) => l.shortlink !== link.shortlink), false);
+              } else {
+                alert('Failed to delete short link');
+              }
+              }}
+              className="text-red-500 hover:underline ml-2"
+            >
+              DELETE
+            </button> */}
             </div>
-          ))
-        }
+          </div>
+          <div className="text-gray-300">
+            {link.redirectUrl.length > 30 ? (
+            <button
+              onClick={(e) => {
+              const target = e.currentTarget;
+              target.textContent = link.redirectUrl;
+              target.classList.remove('hover:underline');
+              target.setAttribute('disabled', 'true');
+              }}
+              className="text-blue-700 hover:underline"
+            >
+              Click to expand
+            </button>
+            ) : (
+            link.redirectUrl
+            )}
+          </div>
+          </li>
+        ))}
+        </ul>
+      </Suspense>
       </div>
     </div>
   );
